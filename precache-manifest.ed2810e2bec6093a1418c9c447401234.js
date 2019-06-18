c579d76b003e09b8abad7460117ceea5a5e886bd
jjuracic1 <j_juracic@yahoo.ca>
2019-06-18 07:54:46 +0200

    Updates

---
+++ precache-manifest.ed2810e2bec6093a1418c9c447401234.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c11517c267fc9db7181a5f11baaf5ef3",
+    "url": "/contact-manager/index.html"
+  },
+  {
+    "revision": "7d2808181e1dfaa4a042",
+    "url": "/contact-manager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "2b9be621a0aa91930505",
+    "url": "/contact-manager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "7d2808181e1dfaa4a042",
+    "url": "/contact-manager/static/js/2.8182eb97.chunk.js"
+  },
+  {
+    "revision": "2b9be621a0aa91930505",
+    "url": "/contact-manager/static/js/main.bddc9c4a.chunk.js"
+  },
+  {
+    "revision": "a25dc548b2853493d42e",
+    "url": "/contact-manager/static/js/runtime~main.b61356a3.js"
+  }
+]);
